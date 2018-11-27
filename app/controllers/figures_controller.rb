a89142d24e3be9f527ccd7a0249fb0911952e9f8
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-27 23:00:37 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,7 +2,7 @@ class FiguresController < ApplicationController
   post '/figures' do
     if params[:title][:name].empty?
       new_title = Title.create(name: params[:title][:name])
-      params[:figure][:title_ids] << new_title.id
+      params[:figure][:title_ids] << new_title.id.to_s
     end
 
     if params[:landmark][:name].empty?
