cd6a18249fb741b42401ee06a383b39c7f6d8802
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-27 23:03:39 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,8 +1,6 @@
 class FiguresController < ApplicationController
   post '/figures' do
-    binding.pry
     unless params[:title][:name].empty?
-      binding.pry
       new_title = Title.create(name: params[:title][:name])
       params[:figure][:title_ids] << new_title.id
     end
@@ -13,7 +11,9 @@ class FiguresController < ApplicationController
 
     titles = params[:figure][:title_ids]&.map { |id| Title.find(id) } || []
     landmarks = params[:figure][:landmark_ids]&.map { |id| Landmark.find(id) } || []
-    figure = Figure.create(name: params[:figure][:name], landmarks: landmarks, titles: titles)
+    figure = Figure.create( name: params[:figure][:name],
+                            landmarks: landmarks,
+                            titles: titles)
     redirect "figures/#{figure.id}"
   end
 
