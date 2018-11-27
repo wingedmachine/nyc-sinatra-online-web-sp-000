4d678c28380e131da50522f91a51588abaa658c2
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-27 23:06:37 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,17 +1,17 @@
 class FiguresController < ApplicationController
   post '/figures' do
+
+    titles = params[:figure][:title_ids]&.map { |id| Title.find(id) } || []
+    landmarks = params[:figure][:landmark_ids]&.map { |id| Landmark.find(id) } || []
+
     unless params[:title][:name].empty?
-      binding.pry
-      new_title = Title.create(name: params[:title][:name])
-      params[:figure][:title_ids] << new_title.id
+      titles << Title.create(name: params[:title][:name])
     end
 
     unless params[:landmark][:name].empty?
 
     end
-
-    titles = params[:figure][:title_ids]&.map { |id| Title.find(id) } || []
-    landmarks = params[:figure][:landmark_ids]&.map { |id| Landmark.find(id) } || []
+    
     figure = Figure.create( name: params[:figure][:name],
                             landmarks: landmarks,
                             titles: titles)
