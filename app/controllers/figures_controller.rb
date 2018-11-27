92701481eca1d02152aee1db4dc008f1104cb028
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-27 22:55:12 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,8 +8,8 @@ class FiguresController < ApplicationController
 
     end
 
-    titles = params[:figure][:title_ids]&.map { |id| Title.find(id) }
-    landmarks = params[:figure][:landmark_ids]&.map { |id| Landmark.find(id) }
+    titles = params[:figure][:title_ids]&.map { |id| Title.find(id) } || []
+    landmarks = params[:figure][:landmark_ids]&.map { |id| Landmark.find(id) } || []
     figure = Figure.create(landmarks: landmarks, titles: titles)
     binding.pry
     redirect "figures/#{figure.id}"
