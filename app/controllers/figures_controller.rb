072eb9db4beec4e40f504f708fe09929061d645d
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-27 23:08:29 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,7 +2,7 @@ class FiguresController < ApplicationController
   get '/figures' do
     erb :'/figures/index'
   end
-  
+
   post '/figures' do
 
     titles = params[:figure][:title_ids]&.map { |id| Title.find(id) } || []
